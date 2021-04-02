c918100d7c76d97dce61fe8636d0a404a4a0eec6
DakotaBuckner07 <61479565+DakotaBuckner07@users.noreply.github.com>
2021-04-02 16:49:39 -0600

    Initial Commit
    Committing the initial repository

---
+++ Assets/Scripts/Engine/Body.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddForce(Vector2 force)
+    {
+
+    }
+
+    public void Step(float dt)
+    {
+        //acceleration = new Vector2(0, -9.81f) + (force / mass);
+    }
+}
---
+++ Assets/Scripts/Engine/World.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public BoolData simulate;
+
+    static World instance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (simulate.value == false) return;
+
+        float dt = Time.deltaTime;
+    }
+}
